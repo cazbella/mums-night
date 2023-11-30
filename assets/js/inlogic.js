57cba95e355767ea9d5bdc1ef861b6e9c1d5bfaa
abigailwright89 <102739142+abigailwright89@users.noreply.github.com>
2023-11-30 19:10:26 +0000

    adds map fuctionality to night out section using api

---
+++ assets/js/inlogic.js
@@ -0,0 +1,8 @@
+
+
+
+
+
+
+// https://{baseURL}/search/{versionNumber}/structuredGeocode.{ext}?key={Your_API_Key}&countryCode={countryCode}&limit={limit}&ofss={ofss}&streetNumber={streetNumber}&streetName={streetName}&crossStreet={crossStreet}&municipality={municipality}&municipalitySubdivision={municipalitySubdivision}&countryTertiarySubdivision={countryTertiarySubdivision}
+// &countrySecondarySubdivision={countrySecondarySubdivision}&countrySubdivision={countrySubdivision}&postalCode={postalCode}&language={language}&extendedPostalCodesFor={extendedPostalCodesFor}&view={view}&mapcodes={mapcodes}&entityTypeSet={entityTypeSet}
